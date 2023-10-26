f980f67a21b1a66d55975164bef85f486240bc40
Elijah Guzman <portalplaya02@gmail.com>
2023-10-26 18:16:16 -0500

    changed home_screen to background_screen

---
+++ lib/screens/background_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/screens/home_screen.dart';
+import 'package:meal_app/widgets/main_drawer.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+  const TabsScreen(this.favoriteMeals, {super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, dynamic>>? _pages;
+  int _selectedPageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    _pages = [
+      {'page': const CategoriesScreen(), 'title': 'Home'},
+    ];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages![_selectedPageIndex]['title']),
+      ),
+      drawer: const MainDrawer(),
+      body: _pages![_selectedPageIndex]['page'],
+    );
+  }
+}
