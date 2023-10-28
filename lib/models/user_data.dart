292d62239effa079d18177d844b0e5c2611cd8e3
Elijah Guzman <portalplaya02@gmail.com>
2023-10-27 23:12:18 -0500

    changed the name of some variables because I hate typing "_". ah ha ha Xd

--- lib/models/user_data.dart
+++ lib/models/user_data.dart
@@ -2,16 +2,16 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class UserSettings extends ChangeNotifier {
-  String _name = '';
-  DateTime _dateOfBirth = DateTime.now();
-  double _feet = 0.0;
-  double _inches = 0.0;
+  String name = '';
+  DateTime dateOfBirth = DateTime.now();
+  double feet = 0.0;
+  double inches = 0.0;
 
   void updateSettings(String name, DateTime dateOfBirth, double feet, double inches) {
-    this._name = name;
-    this._dateOfBirth = dateOfBirth;
-    this._feet = feet;
-    this._inches = inches;
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+    this.feet = feet;
+    this.inches = inches;
     notifyListeners();
   }
 }
